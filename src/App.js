ee706e5ec19b84370b72d66329c1a86e6621dc9d
Prabhat <singhprabhatp.s.224@gamil.com>
2021-11-22 17:49:02 +0530

    Adding reducer in the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { Container } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+import TodoContext from './Context/TodoContext'
+import TodoReducer from './Context/reducer'
+
 
 const App = () => {
+  const [todos, dispatch] = useReducer(TodoReducer, [])
   return (
-    <h1>Todo List with Context API</h1>
+    <div>
+      <TodoContext.Provider value={{ todos, dispatch }}>
+        <Container fluid>
+          <h1>Todo List with Context API</h1>
+        </Container>
+      </TodoContext.Provider>
+    </div>
   )
 }
 
